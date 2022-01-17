7742528feb2c01dd3491ebff77ac2c4a02cc8002
Patrick Villagrán <patrick.villagran@reign.cl>
2022-01-17 14:29:22 -0300

    feat(faaaaaaa): addsdsds

--- apps/api/payment-service/src/app/app.service.ts
+++ apps/api/payment-service/src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return { message: 'Welcome to payment-service cambio 13' };
+    return { message: 'Welcome to payment-service cambio 14' };
   }
 }
